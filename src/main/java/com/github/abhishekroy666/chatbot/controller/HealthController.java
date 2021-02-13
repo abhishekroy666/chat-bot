4759c95d358319a4f963eb12741ce8588e8f0731
abhishekroy666 <roy.ironmaiden@gmail.com>
2021-02-13 18:30:20 +0530

    refactored HealthController

--- src/main/java/com/github/abhishekroy666/chatbot/controller/HealthController.java
+++ src/main/java/com/github/abhishekroy666/chatbot/controller/HealthController.java
@@ -1,15 +1,18 @@
 package com.github.abhishekroy666.chatbot.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
+@RequestMapping("/health")
 public class HealthController {
 
-    @GetMapping(value = "/health")
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> health(){
         return ResponseEntity.ok().build();
     }
