aca28aae76963db9241455a6fe608155b4ac3889
abhishekroy666 <roy.ironmaiden@gmail.com>
2022-07-21 01:10:24 +0530

    Re-factored ChatServiceImpl

--- src/main/java/com/github/abhishekroy666/chatbot/service/impl/ChatServiceImpl.java
+++ src/main/java/com/github/abhishekroy666/chatbot/service/impl/ChatServiceImpl.java
@@ -8,7 +8,6 @@ import com.github.abhishekroy666.chatbot.service.SentenceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,7 +17,9 @@ import java.util.Random;
 @Service
 public class ChatServiceImpl implements ChatService {
 
-    private static final Map<SentenceType, Integer> lastUsedIdxMap = new HashMap<>();
+    private static final Map<SentenceType, Integer> LAST_USED_RESPONSE_MAP = new HashMap<>();
+
+    private final Random random = new Random();
 
     @Autowired
     private SentenceService sentenceService;
@@ -38,7 +39,7 @@ public class ChatServiceImpl implements ChatService {
 
     private Optional<String> createResponse(Message message) {
         final SentenceType sentenceType = MessageResponseSentenceTypeClassifier.classify(message);
-        final boolean prefixable = !sentenceType.equals(SentenceType.ANONYMOUS) && !sentenceType.equals(SentenceType.BLANK);
+        final boolean prefixable = !sentenceType.equals(SentenceType.ANONYMOUS);
         return this.randomize(this.sentenceService.retrieve(sentenceType))
                 .map(sentence -> {
                     if (prefixable) {
@@ -54,28 +55,60 @@ public class ChatServiceImpl implements ChatService {
     private Optional<SentenceModel> randomize(List<SentenceModel> sentences) {
         Optional<SentenceModel> sentenceModel = Optional.empty();
         if (sentences != null && !sentences.isEmpty()) {
-            sentenceModel = RandomUtils.randomSentenceModel(sentences);
+            sentenceModel = this.fetchRandomSentenceModel(sentences);
             if (sentenceModel.isPresent()) {
                 final SentenceType sentenceType = sentenceModel.get().getSentenceType();
-                if (lastUsedIdxMap.containsKey(sentenceType)) {
-                    final int lastIdx = lastUsedIdxMap.get(sentenceType);
-                    while (sentenceModel.isPresent() && sentenceModel.get().getId() == lastIdx) {
-                        sentenceModel = RandomUtils.randomSentenceModel(sentences);
+                if (LAST_USED_RESPONSE_MAP.containsKey(sentenceType)) {
+                    final int prevRespId = LAST_USED_RESPONSE_MAP.get(sentenceType);
+                    while (sentenceModel.isPresent() && sentenceModel.get().getId() == prevRespId) {
+                        sentenceModel = this.fetchRandomSentenceModel(sentences);
                     }
                 }
             }
         }
-        sentenceModel.ifPresent(sentence -> lastUsedIdxMap.put(sentence.getSentenceType(), sentence.getId()));
+        sentenceModel.ifPresent(sentence -> LAST_USED_RESPONSE_MAP.put(sentence.getSentenceType(), sentence.getId()));
         return sentenceModel;
     }
 
+    private Optional<SentenceModel> fetchRandomSentenceModel(List<SentenceModel> sentences) {
+        final int idx = this.random.nextInt(sentences.size());
+        return Optional.of(sentences.get(idx));
+    }
+
     private String applyPrefix(String text, String prefix, Message message) {
-        if (RandomUtils.randomBoolean()) {
-            if (RandomUtils.randomBoolean()) {
-                prefix += " " + RandomUtils.randomizeName(message.getName()) + ",";
+        if (this.random.nextBoolean()) {
+            if (this.random.nextBoolean()) {
+                prefix += " " + this.randomizeName(message.getName()) + ",";
             }
             if (prefix.length() < text.length()) {
-                text = prefix + " " + TextUtils.makePrefixable(text);
+                text = prefix + " " + this.makePrefixable(text);
+            }
+        }
+        return text;
+    }
+
+    public String randomizeName(String name) {
+        if (name != null && !name.isEmpty() && this.random.nextBoolean()) {
+            final String[] nameParts = name.split(" ");
+            int idx = random.nextInt(nameParts.length);
+            String part = nameParts[idx];
+            return (part != null) ? part : name;
+        }
+        return name;
+    }
+
+    private String makePrefixable(String text) {
+        if (text != null && !text.isEmpty()) {
+            final String[] words = text.split(" ");
+            if (words.length >= 1) {
+                final char[] firstWord = words[0].toCharArray();
+                if (firstWord.length > 1) {
+                    final char[] chars = text.toCharArray();
+                    char ch = chars[0];
+                    ch = ("" + ch).toLowerCase().charAt(0);
+                    chars[0] = ch;
+                    text = new String(chars);
+                }
             }
         }
         return text;
@@ -94,45 +127,6 @@ public class ChatServiceImpl implements ChatService {
                 sentenceType = SentenceType.STATEMENT;
             }
             return sentenceType;
-        }
-    }
-
-    private static class TextUtils {
-        public static String makePrefixable(String text) {
-            if (text != null && !text.isEmpty()) {
-                final char[] chars = text.toCharArray();
-                char ch = chars[0];
-                ch = ("" + ch).toLowerCase().charAt(0);
-                chars[0] = ch;
-                text = new String(chars);
-            }
-            return text;
-        }
-    }
-
-    private static class RandomUtils {
-
-        private static final Random random = new Random();
-
-        public static boolean randomBoolean() {
-            return (random.nextInt() % 2) != 0;
-        }
-
-        public static Optional<SentenceModel> randomSentenceModel(List<SentenceModel> sentences) {
-            Collections.shuffle(sentences);
-            return sentences.stream().findFirst();
-        }
-
-        public static String randomizeName(String name) {
-            if (name != null && !name.isEmpty()) {
-                if (randomBoolean()) {
-                    final String[] nameParts = name.split(" ");
-                    int idx = random.nextInt(nameParts.length);
-                    String part = nameParts[idx];
-                    return (part != null) ? part : name;
-                }
-            }
-            return name;
         }
     }
 }
