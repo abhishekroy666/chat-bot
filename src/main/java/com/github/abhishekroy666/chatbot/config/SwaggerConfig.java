205dfd91f594a04643dae4495453ef2be40cbfe9
abhishekroy666 <roy.ironmaiden@gmail.com>
2022-08-09 03:03:07 +0530

    Added security.

--- src/main/java/com/github/abhishekroy666/chatbot/config/SwaggerConfig.java
+++ src/main/java/com/github/abhishekroy666/chatbot/config/SwaggerConfig.java
@@ -5,9 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -17,6 +24,8 @@ public class SwaggerConfig {
     public Docket docket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(this.apiInfo())
+                .securityContexts(Collections.singletonList(securityContext()))
+                .securitySchemes(Collections.singletonList(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.github.abhishekroy666.chatbot.resource.api"))
                 .build();
@@ -29,5 +38,20 @@ public class SwaggerConfig {
                 .description("This page lists all the APIs for chat-bot")
                 .version("0.0.1-SNAPSHOT")
                 .build();
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Collections.singletonList(new SecurityReference("JWT", authorizationScopes));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
     }
 }
---
+++ src/main/java/com/github/abhishekroy666/chatbot/resource/AuthenticationController.java
@@ -0,0 +1,52 @@
+package com.github.abhishekroy666.chatbot.resource;
+
+import com.github.abhishekroy666.chatbot.config.security.JwtTokenizer;
+import com.github.abhishekroy666.chatbot.model.JwtRequest;
+import com.github.abhishekroy666.chatbot.model.JwtResponse;
+import com.github.abhishekroy666.chatbot.service.JwtUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Abhishek Roy
+ */
+@RestController
+@CrossOrigin
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenizer jwtTokenizer;
+
+    @Autowired
+    private JwtUserDetailsService userDetailsService;
+
+    @PostMapping(value = "/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest jwtRequest) throws Exception {
+        this.authenticate(jwtRequest.getUsername(), jwtRequest.getPassword());
+        final UserDetails userDetails = this.userDetailsService.loadUserByUsername(jwtRequest.getUsername());
+        final String token = this.jwtTokenizer.generateToken(userDetails);
+        return ResponseEntity.ok(new JwtResponse(jwtRequest.getUsername(), token));
+    }
+
+    private void authenticate(String username, String password) throws Exception {
+        try {
+            this.authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (DisabledException exception) {
+            throw new Exception("USER_DISABLED", exception);
+        } catch (BadCredentialsException exception) {
+            throw new Exception("INVALID_CREDENTIALS", exception);
+        }
+    }
+}
